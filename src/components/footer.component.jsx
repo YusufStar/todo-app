73467ed261437cd9f0da78d45db6072f2fac578c
Yusuf Yıldız <ysfyldz0077@gmail.com>
2022-10-20 11:54:40 +0300

    done

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <span>Drag and drop to reorder list</span>
             <div className="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-                Coded by <a href="https://www.frontendmentor.io/profile/Fernando0654" target="_blank">FernandoFeN</a>.
+                Coded by <a href="https://www.frontendmentor.io/profile/YusufStar" target="_blank">YusufStar</a>.
             </div>
         </div>
     )
